ac443910d3b1c5f05e06623ccbf7fee582b160f4
Juan Cruz <juancbonelli@gmail.com>
2026-01-31 13:52:45 +0100

    creacion de archivos, rutas y middlewares

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const routes = require("./routes/routes");
+const horaMiddleware = require("./middleware/horaMiddleware");
+const PORT = 3000;
+
+app.use(horaMiddleware);
+
+app.use("/", routes);
+
+app.use((req, res) => {
+  res.status(404).send("<h1>404 Not found</h1><p>La página no existe</p>");
+});
+
+app.listen(PORT, () => {
+  console.log(`El servidor está escuchando en el puerto ${PORT}`);
+});
